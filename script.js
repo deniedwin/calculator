f2544aa7d61f22e929ab08280e280f73a622955f
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-12 12:13:01 +0100

    Made switch case to use all operators

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 };
+
+const varA = 0;
+const varB = 0;
+const optor = 0;
+
+function operate(op, a, b){
+    
+    switch(op){
+        case "+": 
+            console.log("add here");
+            add(a,b);
+            break;
+        case "-":
+            console.log("substract here");
+            subtract(a,b);
+            break;
+        case "*":
+            console.log("multiply here");
+            multiply(a,b);
+            break;
+        case "/": 
+            console.log("divide here");
+            divide(a,b);
+            break;
+        default:
+            console.log("enter other op");
+    }
+}
